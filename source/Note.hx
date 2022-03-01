aca1a34284648ea1db6d5008a4747c4fb04e7cfa
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-03-01 19:14:06 -0300

    Update Note.hx

--- source/Note.hx
+++ source/Note.hx
@@ -102,7 +102,7 @@ class Note extends FlxSprite
 					colorSwap.hue = 0;
 				        colorSwap.saturation = 0;
 					colorSwap.brightness = 0;
-					if(isPresentNote) 
+					if(isSustainNote) 
 				case 'No Animation':
 					noAnimation = true;
 			}
